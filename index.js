7b3b65238d849a6b34373348711214d1b554e9cf
Nico <nico.vbiljon@gmail.com>
2024-09-02 19:45:43 +0200

    Update index.js
    Created 2 more variables called "hasBlackJack" and "isAlive"
    Added conditional statements

--- index.js
+++ index.js
@@ -5,7 +5,21 @@
 
 let firstCard = 6
 let secondCard = 9
-
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Congratulations! You've got Blackjack! 🥳")
+    hasBlackJack = true
+} else {
+    console.log("Sorry, You're out of the game! 😭")
+    isAlive = false
+}
+
+// Cash Out!
+
 
 console.log(sum)
